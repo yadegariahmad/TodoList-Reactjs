27092737ea1c5d33a06c0186a5ee3708d3a006e0
A.Yadegari <yadegariahmad@gmail.com>
2019-07-23 12:03:59 +0430

    add action types

---
+++ src/store/actionTypes.js
@@ -0,0 +1,10 @@
+// Todo Actions
+export const GET_TODOS = 'GET-TODOS';
+export const ADD_TODO = 'ADD-TODO';
+export const EDIT_TODO = 'EDIT-TODO';
+export const DELETE_TODO = 'DELETE-TODO';
+export const TOGGLE_TODO = 'COMPLETE-TODO';
+
+// Auth Actions
+export const LOG_IN = 'LOG-IN';
+export const SIGN_UP = 'SIGN-UP';
