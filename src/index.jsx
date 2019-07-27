84766f37464762a8db27db27fb0846aff7b5fdc2
A.Yadegari <yadegariahmad@gmail.com>
2019-07-27 08:06:46 +0430

    add i18n

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import store from './store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './utils/i18n';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 
--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,10 +1,10 @@
 import i18n from 'i18next';
 import Backend from 'i18next-xhr-backend';
-import { reactI18nextModule } from 'react-i18next';
+import { initReactI18next } from 'react-i18next';
 
 i18n
   .use(Backend)
-  .use(reactI18nextModule)
+  .use(initReactI18next)
   .init({
     fallbackLng: 'en',
     backend: {
